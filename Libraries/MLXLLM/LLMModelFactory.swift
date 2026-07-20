c3ccd0c0767979b0a513d1b6c1e44720848b17e2
David Koski <dkoski@apple.com>
2026-07-20 13:13:25 -0700

    redo ModelContainer -- mostly do not break source compatibility

--- Libraries/MLXLLM/LLMModelFactory.swift
+++ Libraries/MLXLLM/LLMModelFactory.swift
@@ -521,7 +521,6 @@ private struct LLMUserInputProcessor: UserInputProcessor {
 public final class LLMModelFactory: GenericModelFactory, TrainableModelContextLoader {
 
     public typealias ContextType = ModelContext
-    public typealias ContainerType = ModelContainerConstraint
 
     public init(
         typeRegistry: ModelTypeRegistry<TrainableLanguageModel>,
